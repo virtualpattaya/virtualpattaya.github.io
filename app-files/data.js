250739860bf76e473364ead6e92051ae40af6416
Billpearson75 <37235709+Billpearson75@users.noreply.github.com>
2018-09-22 07:40:03 +1000

    marzipano test

---
+++ app-files/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-big-buddha-pattaya",
+      "name": "Big Buddha Pattaya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.046617532636620496,
+        "pitch": -0.023699344702095004,
+        "fov": 1.2768021958253253
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Big Buddha",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
